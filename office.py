b8729638ff86671da3970e6d0b699dcbbf826db6
JonathanCecil01 <jonathansesuraj@gmail.com>
2023-05-05 12:16:36 +0530

    Reader animation of distance generation is done

--- office.py
+++ office.py
@@ -1,18 +1,100 @@
 import matplotlib.pyplot as plt
 import pygame
+from tags import LandMark, Product, Reader
+from copy import deepcopy
+import random
+import math
+
 
 pygame.init()
+N = 100
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+COUNT = 10
 
-screen = pygame.display.set_mode((400, 500))
+colors = ["red", "blue", "brown", "black", "purple"]
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill((255, 255, 255))
+
+def plot_Landmarks(count):
+    landmarks = []
+    steps_x  = SCREEN_WIDTH/5
+    steps_y  = SCREEN_HEIGHT/2
+    loc1 = [200,250]
+    flag = True
+    for i in range(count-1):
+        loc = deepcopy(loc1)
+        l = LandMark("L"+str(i), loc)
+        landmarks.append(l)
+        if i%2==0 and i!=0:
+            loc1[0]+=steps_x
+        if i%2!=0:
+            if flag:
+                loc1[1]+=steps_y
+                flag = False
+            else:
+                loc1[1]-=steps_y
+                flag = True
+    landmarks.pop(0)
+    i = 0
+    for landmark in landmarks:
+        landmark.draw(screen, colors[i%5])
+        print(landmark.location)
+        i+=1
+    return landmarks
+
+def plot_products():
+    products = []
+    iterations= 100
+    for i in range(iterations):
+        i = random.randint(0, 1)
+        if i == 1:
+            location = [random.randint(150, 850), random.randint(150, 450)]
+        else:
+            location = [random.randint(150, 850), random.randint(600, 850)]
+        product  = Product("P"+str(random.randint(0, N)), location)
+        
+        products.append(product)
+    for product in products:
+        product.draw(screen, "green")
+    return products
+
+
+
 
 flag = True
 i = 5
+distance_products = []
+distance_landmarks = []
+products = plot_products()
+landmarks = plot_Landmarks(COUNT)
+reader = Reader([0, 0], 30)
+top_surface = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT), pygame.SRCALPHA)
+reader.draw(top_surface)
+screen.blit(top_surface, (0, 0))
 while flag:
-    pygame.draw.rect(screen, (0, 0+i, 255),(200,150,100,50))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             flag = False
             break
-    pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                reader.location[1]+=100
+            if  event.key == pygame.K_a:
+                reader.location[0]-=100
+            if event.key == pygame.K_d:
+                reader.location[0]+=100
+            if event.key == pygame.K_w:
+                reader.location[1]-=100
+            top_surface.fill((255, 255, 255, 0))
+            reader.draw(top_surface)
+            screen.blit(top_surface, (0, 0))
+    for product in products:
+        if math.dist(product.location, reader.location)<=200:
+            product.distances.append(math.dist(product.location, reader.location))
+    for landmark in landmarks:
+        if math.dist(landmark.location, reader.location)<=200:
+            landmark.distances.append(math.dist(landmark.location, reader.location))
+    pygame.display.flip()
     i+=1
+
--- tags.py
+++ tags.py
@@ -1,5 +1,99 @@
+import pygame
+import random
+# pygame.init()
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
 
-class Tags:
+PATH_LOSS_EXPONENT = 2
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# screen.fill((255, 255, 255))
+
+class Reader:
+    def __init__(self, location, radius):
+        self.location = location
+        self.radius = radius
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface , (30,224,33,20), self.location, 250)
+        # screen.blit(surface, (0, 0))
+        pygame.draw.circle(surface, "black", self.location, 5)
+        #pygame.display.update()
+
+class Tag:
     def __init__(self, location):
         self.location = location
+    def draw(self, screen, color):
+        pygame.draw.circle(screen, color, self.location, 10)
+        pygame.display.update()
+        return
+
+class Product(Tag):
+    def __init__(self, id, location):
+        Tag.__init__(self, location)
+        self.id = id
+        self.rssi_A = random.randint(-90,-30)
+        self.timestamp = []
+        self.distances = []
+        self.rssi = []
+        self.max_rssi = -81
+        self.timestamp_range = []
+
+    def find_max_rssi(self):
+        pass
+
+
+class LandMark(Tag):
+    def __init__(self, id, location):
+        Tag.__init__(self, location)
+        self.id = id
+        self.timestamp = []
+        self.rssi = []
+        self.distances  = []
+        self.max_rssi = -81
+        self.timestamp_range = []
+    
+    def find_max_rssi(self):
+        pass
+
+landmarks = []
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# screen.fill((255, 255, 255))
+
+# def plot_Landmarks(count):
+#     steps_x  = SCREEN_WIDTH/5
+#     steps_y  = SCREEN_HEIGHT/2
+#     loc = [200,250]
+#     flag = True
+#     for i in range(count-1):
+#         l = LandMark("L"+str(i), loc)
+#         l.draw()
+#         landmarks.append(l)
+#         if i%2==0:
+#             loc[0]+=steps_x
+#         if i%2!=0:
+#             if flag:
+#                 loc[1]+=steps_y
+#                 flag = False
+#             else:
+#                 loc[1]-=steps_y
+#                 flag = True
+#     for landmark in landmarks:
+#         print(landmark.location)
+#         landmark.draw()
+
+
+
+# flag = True
+# i = 5
+
+
+
+# while flag:
+#     plot_Landmarks(10)
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             flag = False
+#             break
+#     pygame.display.update()
+#     i+=1
