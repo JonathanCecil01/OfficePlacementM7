065a8bb286725d7b026646576f619d1fadf77f3a
JonathanCecil01 <jonathansesuraj@gmail.com>
2023-07-20 20:40:08 +0530

    Multiple readers implemented

---
+++ ContextSearching/hayStack_implementation/README.txt
@@ -0,0 +1,16 @@
+The Objectve of this application is to identify the location of the Texts with the highest context match with an input Query.
+
+This follows the extractive QnA technique to Highlight the highest context match in a given pdf file.
+
+Haystack Library is used to create a retriever reader pipeline for the extraction.
+Retriever - BM25
+Reader Model - Roberta Uncased
+
+Steps to Run the application:
+
+1. Insert the file of interest in the data folder to perform extraction
+2. install the 'nltk' library, install the complete version of haystack
+3. Then run the program from the existing directory. 
+4. A faiss document db, a results json file and a new pdf with the highlights will be created.
+
+Note: before rerunning the application ensure to delete the existing db.
--- ContextSearching/hayStack_implementation/haystack_based_context_search.py
+++ ContextSearching/hayStack_implementation/haystack_based_context_search.py
@@ -2,6 +2,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from multiprocessing import freeze_support
 
+import pdfplumber
 import nltk
 nltk.data.path.append("ContextSearching/hayStack_implementation/nltk_data")
 nltk.download('punkt')
@@ -14,20 +15,43 @@ from haystack.nodes import FARMReader
 from haystack.pipelines import ExtractiveQAPipeline
 from haystack.utils import print_answers
 import fitz
+import re
+
+def extract_number_from_string(input_string):
+    numbers = re.findall(r'\d+', input_string)
+    return int(numbers[0]) if numbers else None
 
 def highlight_pdf(pdf_path, sentences):
     doc = fitz.open(pdf_path)
-    for page in doc:
-        for sentence in sentences:
-            search_results = page.search_for(sentence)
-            for rect in search_results:
+    for sentence in sentences:
+        page = sentence[1]
+        context = sentence[0]
+        search_results = page.search_for(context)
+        for rect in search_results:
                 highlight = page.add_highlight_annot(rect)
     output_file = "ContextSearching/hayStack_implementation/n_file.pdf"
     doc.save(output_file)
     doc.close()
 
+def pdf_to_text_with_structure(pdf_file_path):
+    text = []
+    with pdfplumber.open(pdf_file_path) as pdf:
+        for page in pdf.pages:
+            text .append(page.extract_text()+'\n')
+            #text+='\n'
+
+    return text
+
+
 
 if __name__ == "__main__":
+
+    pdf_path = "ContextSearching/hayStack_implementation/SampleContent2LPA.pdf"
+    pages_arr = pdf_to_text_with_structure(pdf_path)
+    for i in range(len(pages_arr)):
+        file = "ContextSearching/hayStack_implementation/data/doc" + str(i)+".txt"
+        with open(file, 'w') as f:
+            f.write(pages_arr[i])
 
     doc_dir = "ContextSearching/hayStack_implementation/data"
 
@@ -48,16 +72,62 @@ if __name__ == "__main__":
     )
     document_store.update_embeddings(retriever)
 
+    #reader = FARMReader(model_name_or_path="ahotrod/albert_xxlargev1_squad2_512", use_gpu=False)
+
+    #reader = FARMReader(model_name_or_path="deepset/roberta-large-squad2", use_gpu=False)
+    
     reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
 
     pipe = ExtractiveQAPipeline(reader, retriever)
 
 
-    prediction = pipe.run(
-        query="How much is the Main Fund?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
-    )
+    # prediction = pipe.run(
+    #     query="What is the Fax Number?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+    # )
 
-    queries = ["what is the Fund Name?", "Start Date?", "Which Section has Carried Interest?", "Who is the General Partner?", "When is the Initial Closing Date?", "When is the Final Closing Date?", "Which is the Management Company?", "What are the Investment Limitations?", "Which sections has the Purpose?", "How long is the Partnership Term?", "How much is the Main Fund?", "How Much is the Transaction Fees?", "How much is the Makeup Contribution?"]
+    #Queries for SearchContextTest1
+    queries = [
+    "What is the Date?",
+    "What is the Applicant Name?",
+    "Who is the General Partner Name?",
+    "Who is the Limited Partner Name?",
+    "What is the Fund Name?"
+    "What is the Law Firm?",
+    "What is the Fund Address?",
+    "What is the Fund Domicile?",
+    "How much is the Commitment Amount?",
+    "What is the Vehicle?",
+    "What is the Telephone Number?",
+    "What is the Fax Number?",
+    "What is the Email Address?",
+    "What is the Tax ID Number?"
+    ]
+
+    #Queries for Equity
+    # queries = [
+    # "What is the Investment Name?",
+    # "Who is the General Partner?",
+    # "Who is the Limited Partner?",
+    # "What is the Fund Name?",
+    # "What is the Fund Address?",
+    # "How much is the Investment Amount?",
+    # "When is the Notice Date?",
+    # "When is the Due Date?",
+    # "What is the Currency Code?",
+    # "What is the Amount Due?",
+    # "What is the Management Fees?",
+    # "What is the Total Capital Contribution?",
+    # "What is the Bank Name?",
+    # "What is the Account Name?",
+    # "How muc us the Partnership Expenses?",
+    # "how much is the Management Fee Discount?",
+    # "How much is the Total Due from Investors?",
+    # "What is the Undrawn Commitment Before?",
+    # "What is the Undrawn Commitment After?",
+    # "Excess Cash returned?",
+    # "What is the  Number for the Bank Account?",
+    # "What is the Address of the Bank?"
+    # ]
     predictions = {}
     all_results = []
     for query in queries:
@@ -67,15 +137,17 @@ if __name__ == "__main__":
             temp_dict = {}
             temp_dict['answer'] = i.answer
             temp_dict['context'] = i.context
-            temp_dict['filename'] = i.meta['name']
+            temp_dict['filename'] = "SearchContextTest1.pdf"
+            temp_dict['page_no'] = extract_number_from_string(i.meta['name'])
             temp_dict['score'] = i.score
             predictions[query].append(temp_dict)
-            print(i.answer)
-            all_results.append(i.answer)
+            #print(i.answer)
+            all_results.append([i.context, temp_dict['page_no']])
             # highlight_pdf("ContextSearching/hayStack_implementation/data/SampleContent2LPA.pdf", i.answer)
-    highlight_pdf("ContextSearching/hayStack_implementation/data/SampleContent2LPA.pdf", all_results)
+    document_store.delete_all_documents()
+    highlight_pdf("ContextSearching/hayStack_implementation/SampleContent2LPA.pdf", all_results)
     import json
     json_string = json.dumps(predictions, indent = 2)
-    with open("context_search_results.json", "w") as f:
+    with open("ContextSearching/hayStack_implementation/context_search_results.json", "w") as f:
         f.write(json_string)
     
